43ead4478ca9c9b54b72fe8fb6989126fec8138e
LAPTOP-9D74L3IT\oweny <owenyi3207@gmail.com>
2020-11-20 01:06:02 +0900

    parameter

--- flask/flaskapp/__init__.py
+++ flask/flaskapp/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from flaskapp.config.dbConnection import db
 
@@ -15,4 +15,13 @@ def helloworld():
 def dbHello():
     cursor = mongo.db.tb_diets.find({}, {'_id': 0})
     list_cur = list(cursor)
+    return jsonify(list_cur)
+
+# host:port/parameter?argument='argument'
+@app.route("/parameter", methods=['GET'])
+def dbKeyword():
+    argument = request.args.get('argument')
+    cursor = mongo.db.tb_diets.find({}, {'_id': 0})
+    list_cur = list(cursor)
+    print(argument)
     return jsonify(list_cur)
