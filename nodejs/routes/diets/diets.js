4fe05bafc13df51f9534eb26cdbd4c46471215e0
choikanghee <rkdgml1106@gmail.com>
2020-12-02 01:12:11 +0900

    modify diets

--- nodejs/routes/diets/diets.js
+++ nodejs/routes/diets/diets.js
@@ -1,7 +1,7 @@
 const dietsModel = require('./dietsModel.js');
 const query = require('./dietsQuery.js');
-const RATE_FIRST = 0.5;
-const RATE_SECOND = 0.3;
+const RATE_FIRST = 0.7;
+const RATE_SECOND = 0.1;
 
 let dailyIntakes = new Object();
 let dailyStandard = new Object();
@@ -115,7 +115,6 @@ exports.postRiceDiets = async(req, res) => {
                     returnJson.res_state = "success";
                     returnJson.res_msg = "밥 데이터를 가져왔습니다.";
                     returnJson.res_data.Rice = result[0];
-                    console.log(result[0].dietName);
                 })
                 .catch(() => {
                     returnJson.res_state = "sql_error";
@@ -213,8 +212,13 @@ exports.postSoupDiets = async(req, res) => {
                         returnJson.res_msg = "잠시 후에 시도해주세요.";
                         res.send(returnJson);
                     });
-                } while(swipeSoup.indexOf(returnJson.res_data.soup.dietName) != -1 || dailyIntakes.kcal - returnJson.res_data.soup.kcal < 0 || dailyIntakes.protein - returnJson.res_data.soup.protein < 0 || dailyIntakes.folate - returnJson.res_data.soup.folate < 0 || dailyIntakes.calcium - returnJson.res_data.soup.calcium < 0 || dailyIntakes.ferrum - returnJson.res_data.soup.ferrum < 0);
-            res.send(returnJson);
+                } while(swipeSoup.indexOf(returnJson.res_data.soup.dietName) != -1 || 
+                dailyIntakes.kcal - returnJson.res_data.soup.kcal < 200 || 
+                dailyIntakes.protein - returnJson.res_data.soup.protein < 7 || 
+                dailyIntakes.folate - returnJson.res_data.soup.folate < 50 || 
+                dailyIntakes.calcium - returnJson.res_data.soup.calcium < 90 || 
+                dailyIntakes.ferrum - returnJson.res_data.soup.ferrum < 2);
+                res.send(returnJson);
 
     } catch(e) {
         console.error(e);
@@ -317,7 +321,6 @@ exports.postSideDiets1 = async(req, res) => {
                     returnJson.res_state = "success";
                     returnJson.res_msg = "반찬을 가져왔습니다.";
                     returnJson.res_data.side = result[0];
-                    console.log(result[0].dietName);
                 })
                 .catch(() => {
                     returnJson.res_state = "sql_error";
@@ -325,12 +328,12 @@ exports.postSideDiets1 = async(req, res) => {
                     res.send(returnJson);
                 });
             } while(swipeSide.indexOf(returnJson.res_data.side.dietName) != -1 || 
-            Math.floor(dailyIntakes.kcal - returnJson.res_data.side.kcal) > dailyStandard.kcal * RATE_FIRST ||
-            Math.floor(dailyIntakes.protein - returnJson.res_data.side.protein) > dailyStandard.protein * RATE_FIRST ||
-            Math.floor(dailyIntakes.folate - returnJson.res_data.side.folate) > dailyStandard.folate * RATE_FIRST ||
-            Math.floor(dailyIntakes.calcium - returnJson.res_data.side.calcium) > dailyStandard.calcium * RATE_FIRST ||
-            Math.floor(dailyIntakes.ferrum - returnJson.res_data.side.ferrum) > dailyStandard.ferrum * RATE_FIRST);
-        res.send(returnJson);
+            Math.abs(dailyIntakes.kcal - returnJson.res_data.side.kcal) > dailyStandard.kcal * RATE_FIRST ||
+            Math.abs(dailyIntakes.protein - returnJson.res_data.side.protein) > dailyStandard.protein * RATE_FIRST ||
+            Math.abs(dailyIntakes.folate - returnJson.res_data.side.folate) > dailyStandard.folate * RATE_FIRST ||
+            Math.abs(dailyIntakes.calcium - returnJson.res_data.side.calcium) > dailyStandard.calcium * RATE_FIRST ||
+            Math.abs(dailyIntakes.ferrum - returnJson.res_data.side.ferrum) > dailyStandard.ferrum * RATE_FIRST);
+            res.send(returnJson);
 
     } catch(e) {
         console.error(e);
@@ -447,7 +450,6 @@ exports.postSideDiets2 = async(req, res) => {
                     returnJson.res_state = "success";
                     returnJson.res_msg = "반찬을 가져왔습니다.";
                     returnJson.res_data.side = result[0];
-                    console.log(result[0].dietName);
                 })
                 .catch(() => {
                     returnJson.res_state = "sql_error";
@@ -455,11 +457,11 @@ exports.postSideDiets2 = async(req, res) => {
                     res.send(returnJson);
                 });
             } while(swipeSide.indexOf(returnJson.res_data.side.dietName) != -1 || 
-            Math.floor(dailyIntakes.kcal - returnJson.res_data.side.kcal) > dailyStandard.kcal * RATE_SECOND ||
-            Math.floor(dailyIntakes.protein - returnJson.res_data.side.protein) > dailyStandard.protein * RATE_SECOND ||
-            Math.floor(dailyIntakes.folate - returnJson.res_data.side.folate) > dailyStandard.folate * RATE_SECOND ||
-            Math.floor(dailyIntakes.calcium - returnJson.res_data.side.calcium) > dailyStandard.calcium * RATE_SECOND ||
-            Math.floor(dailyIntakes.ferrum - returnJson.res_data.side.ferrum) > dailyStandard.ferrum * RATE_SECOND);
+            Math.abs(dailyIntakes.kcal - returnJson.res_data.side.kcal) < -30 ||
+            Math.abs(dailyIntakes.protein - returnJson.res_data.side.protein) < -17 ||
+            Math.abs(dailyIntakes.folate - returnJson.res_data.side.folate) < -200 ||
+            Math.abs(dailyIntakes.calcium - returnJson.res_data.side.calcium) < -310 ||
+            Math.abs(dailyIntakes.ferrum - returnJson.res_data.side.ferrum) < -50);  
             res.send(returnJson);
 
     } catch(e) {
