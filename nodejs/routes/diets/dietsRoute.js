a7e8ef64cd370d66ec593e7a615cdf1bb09e9a03
LAPTOP-9D74L3IT\oweny <owenyi3207@gmail.com>
2021-03-01 16:29:47 +0900

    add diets api

--- nodejs/routes/diets/dietsRoute.js
+++ nodejs/routes/diets/dietsRoute.js
@@ -11,4 +11,12 @@ router.post('/postSideDiets1', diets.postSideDiets1);
 
 router.post('/postSideDiets2', diets.postSideDiets2);
 
+router.post('/postDietsRatingsInit', diets.postDietsRatingsInit);
+
+router.get('/getAllDiets', diets.getAllDiets);
+
+router.patch('/patchSelectedDietsRatingsInit', diets.patchSelectedDietsRatingsInit);
+
+router.patch('/patchSelectedDietsRatings', diets.patchSelectedDietsRatings);
+
 module.exports = router;
